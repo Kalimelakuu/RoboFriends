56b52745f90247b171563fb278dfa9de1a56f60a
Kalkidan Melaku Hailemariam <kalimelakuu>
2022-08-10 13:13:39 -0500

    Updates

---
+++ precache-manifest.e8624f946846c9b7a22356a7210acdfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fee9ba4da20cdc3c00b",
+    "url": "/RoboFriends/static/css/main.d946da7d.chunk.css"
+  },
+  {
+    "revision": "8fee9ba4da20cdc3c00b",
+    "url": "/RoboFriends/static/js/main.8fee9ba4.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RoboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "dd83e069f07d103b77c5",
+    "url": "/RoboFriends/static/css/2.1c35c3ef.chunk.css"
+  },
+  {
+    "revision": "dd83e069f07d103b77c5",
+    "url": "/RoboFriends/static/js/2.dd83e069.chunk.js"
+  },
+  {
+    "revision": "4cb7496509aa3077e2b1",
+    "url": "/RoboFriends/static/js/runtime~main.4cb74965.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "82d0d5d0237d0b23df51f169bcc9b51e",
+    "url": "/RoboFriends/index.html"
+  }
+];
